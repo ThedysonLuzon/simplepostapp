c7d521521cdd9a9438d00cad9a89f9e11fa4bd4e
Thedyson Luzon <luzon.thedyson@gmail.com@users.noreply.github.com>
2023-04-17 14:05:18 -0400

    resuable component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,12 @@
 const names = ['Thedyson Luzon', 'Troy Luzon'];
 
 export default function Post() {
-
     const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-        <p>Thedyson Luzon</p>
-        <p>I am a student</p>
         <p>{chosenName}</p>
+        <p>I am a student</p>
 
     </div>
 
